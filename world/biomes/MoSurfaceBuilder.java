d42e5d118ce9f4390c30a39aec165dcb0427cf4c
BelgiebYT <82049360+BelgiebYT@users.noreply.github.com>
2021-04-15 15:56:38 +0100

    Update MoSurfaceBuilder.java

--- world/biomes/MoSurfaceBuilder.java
+++ world/biomes/MoSurfaceBuilder.java
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = MoreFeatures.MOD_ID)
-public class MoSurfaceBuilder {
+public  class MoSurfaceBuilder <C extends ISurfaceBuilderConfig> extends net.minecraftforge.registries.ForgeRegistryEntry<SurfaceBuilder<?>> {
 
 
     public static final DeferredRegister<SurfaceBuilder<?>> SURFACE_BUILDER = DeferredRegister.create(ForgeRegistries.SURFACE_BUILDERS, MoreFeatures.MOD_ID);
@@ -19,10 +19,10 @@ public class MoSurfaceBuilder {
     public static final RegistryObject<SurfaceBuilder<SurfaceBuilderConfig>> FRUIT_FIELDS = SURFACE_BUILDER.register("fruit_fields", () -> new FruitFieldsSurfaceBuilder(SurfaceBuilderConfig.field_237203_a_));
 
 
+    public static final SurfaceBuilderConfig MOSS_GRASS_GRASS_CONFIG = new SurfaceBuilderConfig( BlockInit.MOSS_BLOCK.get().getDefaultState(), Blocks.DIRT.getDefaultState(), Blocks.DIRT.getDefaultState());
+
     public static final SurfaceBuilderConfig GRASS_GRASS_GRASS_CONFIG = new SurfaceBuilderConfig(Blocks.GRASS_BLOCK.getDefaultState(), Blocks.DIRT.getDefaultState(), Blocks.DIRT.getDefaultState());
 
-
-
-
-
 }
+
+
